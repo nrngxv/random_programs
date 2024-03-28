baedd02f1f740ae90c35fef4bf7579c804d706d3
nrngxv <nirneet.nishant@gmail.com>
2024-03-28 16:10:51 +0530

    Added the game logic, and rules for the game

--- conway_game.py
+++ conway_game.py
@@ -23,4 +23,33 @@ next_cells.append(column) # adding column value to the main list
 
 # The logic of what should happen
 while True: # the program starts
-    
+    print("\n\n\n\n\n") # separting each line with a new line
+    current_cells = copy.deepcopy(next_cells) # copying properties of next_cells
+
+    for y in range(HEIGHT):
+        for x in range(WIDTH):
+           print(current_cells[x][y], end="")
+        print()
+
+    # calculating the next cell, based on the current one
+    for x in range(HEIGHT):
+        for y in range(WIDTH):
+
+            #% WIDTH/HEIGHT ensure, the coords are between 0 and WIDTH/HEIGHT-1
+            leftcoord = (x - 1) % WIDTH
+            rightcoord = (x + 1) % WIDTH
+            topcoord = (y - 1) % HEIGHT
+            bottomcoord = (y + 1) % HEIGHT
+
+            living_neighbour = 0 # the number of living neighbour
+
+            if current_cells[leftcoord][topcoord] == "#":
+                living_neighbour += 1 # adds a living cell to topleft, if the above condition is true
+
+            if current_cells[x][topcoord] == "#":
+                living_neighbour += 1
+
+
+
+            
+
