b2e5e9da79c715df846d510d77ae0894b32e00ca
nrngxv <nirneet.nishant@gmail.com>
2024-04-25 00:15:25 +0530

    It's alive

---
+++ projects/zigzag.py
@@ -0,0 +1,23 @@
+import time, sys
+
+indent = 0
+indent_increasing = True
+
+try:
+    while True:
+        print(" " * indent, end = "")
+        print("******")
+        time.sleep(0.1)
+
+        if indent_increasing:
+            indent = indent + 1
+            if indent == 20:
+                indent_increasing = False
+
+        else:
+            indent = indent - 1
+            if indent == 0:
+                indent_increasing = True
+
+except KeyboardInterrupt:
+    sys.exit()
