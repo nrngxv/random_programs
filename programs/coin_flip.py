b00856ad7bb678f9bd3b18d8fb655349a4f55e9d
nrngxv <nirneet.nishant@gmail.com>
2024-05-28 21:13:54 +0530

    Error: streaks count exceeds flips input

--- programs/coin_flip.py
+++ programs/coin_flip.py
@@ -1,32 +1,40 @@
 import random
+STREAKS = 0 # main: streak's count
 
-ht_list = [] # List that stores head and tails count
-streak = 0
+def simulating_flips(number_of_flips): #defines, number of flips and
+    # coin_flip = ['H' if random.choice([True, False]) else 'T' for _ in range(number_of_flips)]
+    # this is a little advance way to write this. I tried writing it on my own (phind helped a little)
+    coin_flip = []
+    for i in range(number_of_flips):
+        if random.randint(0,1) == 1:
+            coin_flip.append(1)
+        else:
+            coin_flip.append(0)
 
-for i in range(10000):
-    # this generates heads and tails value and stores it
-    if random.randint(0,1) == 1:
-        h = 1 # stores value
-        ht_list.append(1) # puts value in list
-    else:
-        t = 0
-        ht_list.append(0)
+    return coin_flip
 
-# this that counts the number of streak of 6 heads or tails  that appear in the program        
-    step = 0
-    next_step = 7
-    addone = ht_list[step]
-    addtwo = ht_list[step:next_step]
+def coin_streak(coin_flip):
+    main_streak = 0
+    current_streak = 1
+
+    for i in range(1, len(coin_flip)):
+        if coin_flip[i] == coin_flip[i - 1]:
+            current_streak += 1
+        
+            if current_streak == 6:
+                main_streak += 1
+                current_streak = 1
+
+        else:
+            current_streak = 1
+
+    return print(f"Number of main_ streak: {main_streak}")
+
+flips_command = int(input("Enter number of flips:")) # main: input
+
+
+for _ in range(flips_command):
+    flips = simulating_flips(flips_command)
+    streaks = STREAKS + coin_streak(flips)
     
-'''
-    if ht_list_add == 6:
-        streak = streak + 1
-    else:
-        if ht_list_add == 0:
-            streak = streak + 1
-        else: # increases values in ht_list[]
-            step = step + 1
-            next_step = next_step + 1   
-'''
-print(ht_list) # Prints heads and tails list
-print(streak)
+print(f"The total streaks is", {streaks})
