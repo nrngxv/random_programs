c755a6dbf91b648a82f12965f8a3b63ef745c780
nrngxv <nirneet.nishant@gmail.com>
2024-05-28 21:16:09 +0530

    removed file: visual_math.py

--- programs/visual_math.py
+++
@@ -1,8 +0,0 @@
-'''I want to generate a math + pattern type simulation. A visual representation of a operation.'''
-
-import random, sys, time
-
-# random number pops up, chooses character from the list
-list = ["#","@","%","!"]
-
-'''I want to take 2 inputs, add a operator and then make a image of the squence, also animate it.'''
